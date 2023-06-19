ac09820564b719daeeb9c1adef954ca6d2863e67
Chaitanya Kumar <chaitanya.4294@gmail.com>
2023-06-19 12:08:42 +0530

    Add empty project

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieStoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
